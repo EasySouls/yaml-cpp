6778338eb035df54ee5d2efb3fd78290d9ac198d
EasySouls <tarjanyicsanad@gmail.com>
2025-05-22 00:59:32 +0200

    Add premake file and update gitignore

---
+++ premake5.lua
@@ -0,0 +1,44 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+
+		"include/**.h",
+		"include/yaml-cpp/**.h",
+		"include/yaml-cpp/node/**.h",
+		"include/yaml-cpp/node/detail/**.h",
+	}
+
+	includedirs
+	{
+		"include",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines 
+		{
+			"YAML_CPP_STATIC_DEFINE",
+		}
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+	    runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
