8d3d44cb3f6c127cf53740cecd0a75bff8d97d6e
EasySouls <tarjanyicsanad@gmail.com>
2025-05-22 02:56:38 +0200

    Fix premake

--- premake5.lua
+++ premake5.lua
@@ -1,44 +1,44 @@
 project "yaml-cpp"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++20"
-	staticruntime "off"
+  kind "StaticLib"
+  language "C++"
+  cppdialect "C++20"
+  staticruntime "on"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+  targetdir("bin/" .. outputdir .. "/%{prj.name}")
+  objdir("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
-		"src/**.h",
-		"src/**.cpp",
+  files
+  {
+	  "src/**.h",
+	  "src/**.cpp",
 
-		"include/**.h",
-		"include/yaml-cpp/**.h",
-		"include/yaml-cpp/node/**.h",
-		"include/yaml-cpp/node/detail/**.h",
-	}
+	  "include/**.h",
+	  "include/yaml-cpp/**.h",
+	  "include/yaml-cpp/node/**.h",
+	  "include/yaml-cpp/node/detail/**.h",
+  }
 
-	includedirs
-	{
-		"include",
-	}
+  includedirs
+  {
+	  "include",
+  }
 
-	filter "system:windows"
-		systemversion "latest"
+  defines
+  {
+	  "YAML_CPP_STATIC_DEFINE",
+  }
 
-		defines 
-		{
-			"YAML_CPP_STATIC_DEFINE",
-		}
+  filter "system:windows"
+    systemversion "latest"
 
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
+  filter "system:linux"
+    pic "On"
+    systemversion "latest"
 
-	filter "configurations:Debug"
-	    runtime "Debug"
-		symbols "on"
+  filter "configurations:Debug"
+    runtime "Debug"
+    symbols "on"
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+  filter "configurations:Release"
+    runtime "Release"
+    optimize "on"
